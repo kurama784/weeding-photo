ad708847d48ead788dbc762cdc8b727d56bd13be
prosperodesu <kolabrod@gmail.com>
2014-11-16 23:39:22 +0200

    make it better

--- lira/urls.py
+++ lira/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.conf import settings
 from django.conf.urls.static import static
-from filebrowser.sites import site
 
 from gallery.views import AlbumDetailView, AlbumListView
-
 
 from django.contrib import admin
 admin.autodiscover()
@@ -19,8 +16,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^upload_stat/', 'pages.views.upload_stat', name ='upload_stat'),
     url(r'^status/', 'pages.views.status', name ='status'),
-    (r'^admin/filebrowser/', include(site.urls)),
-    (r'^grappelli/', include('grappelli.urls')),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^tinymce/', include('tinymce.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
